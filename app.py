0b560b52a42d075cd599cda7fd9a01fa34962daa
DanielBruz <daniel.bruzenak@tiscali.cz>
2023-08-18 15:52:41 +0200

    formulare

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api, Resource
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SelectField
+from wtforms import StringField, IntegerField, SelectField, BooleanField
 from wtforms.validators import DataRequired
 import json
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Se\xb1z\x9bD_\xdc\xd3\xc2|\x7f|\xf0\x83Yw\xe2\xf7%=\xf0\xe5'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost:5432/carsdb'
 db = SQLAlchemy(app)
 api = Api(app)
@@ -56,7 +57,8 @@ class CarModelForm(FlaskForm):
     category = StringField('Category')
     price_range = StringField('Price Range')
     release_year = IntegerField('Release Year')
-    is_active = SelectField('Is Active', choices=[(True, 'Yes'), (False, 'No')])
+    is_active = BooleanField('Is Active')
+    manufacturer = SelectField('Manufacturer', coerce=int, validators=[DataRequired()])
 
 
 class CarForm(FlaskForm):
@@ -64,7 +66,18 @@ class CarForm(FlaskForm):
     horsepower = IntegerField('Horsepower')
     consumption = IntegerField('Consumption')
     manufacture_date = StringField('Manufacture Date')
-    is_drivable = SelectField('Is Drivable', choices=[(True, 'Yes'), (False, 'No')])
+    is_drivable = BooleanField('Is Drivable')
+    car_model = SelectField('Car Model', coerce=int, validators=[DataRequired()])
+
+
+class CarFilterForm(FlaskForm):
+    manufacturer = SelectField('Manufacturer', coerce=int)
+    car_model = SelectField('Car Model', coerce=int)
+
+    def __init__(self, *args, **kwargs):
+        super(CarFilterForm, self).__init__(*args, **kwargs)
+        self.manufacturer.choices = [(manufacturer.id, manufacturer.name) for manufacturer in Manufacturer.query.all()]
+        self.car_model.choices = [(car_model.id, car_model.name) for car_model in CarModel.query.all()]
 
 
 # API Resources
@@ -79,6 +92,13 @@ class ManufacturerResource(Resource):
 
     def delete(self, manufacturer_id):
         manufacturer = Manufacturer.query.get(manufacturer_id)
+        if manufacturer:
+            db.session.delete(manufacturer)
+            db.session.commit()
+            flash('Manufacturer deleted successfully', 'success')
+        else:
+            flash('Manufacturer not found', 'danger')
+        return redirect(url_for('index'))
         # Kód pro smazání dat
 
 
@@ -115,15 +135,37 @@ api.add_resource(ManufacturerResource, '/api/manufacturer/<int:manufacturer_id>'
 api.add_resource(CarModelResource, '/api/model/<int:model_id>')
 api.add_resource(CarResource, '/api/car/<int:car_id>')
 
-
 # Hlavní stránka pro zobrazení dat
-@app.route('/')
+from sqlalchemy import or_, and_
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     manufacturers = Manufacturer.query.all()
     car_models = CarModel.query.all()
     cars = Car.query.all()
+
+    form = CarFilterForm()
+
+    if form.validate_on_submit():
+        manufacturer_id = form.manufacturer.data
+        car_model_id = form.car_model.data
+
+        if manufacturer_id and car_model_id:
+            cars = Car.query.join(CarModel).filter(
+                and_(Car.model_id == car_model_id, CarModel.manufacturer_id == manufacturer_id)
+            ).all()
+        elif manufacturer_id:
+            cars = Car.query.join(CarModel).filter(
+                CarModel.manufacturer_id == manufacturer_id
+            ).all()
+        elif car_model_id:
+            cars = Car.query.filter_by(model_id=car_model_id).all()
+        else:
+            cars = Car.query.all()
+
     return render_template('index.html', manufacturers=manufacturers,
-                           car_models=car_models, cars=cars)
+                           car_models=car_models, cars=cars, form=form)
 
 
 # Přidání/úprava výrobce
@@ -131,8 +173,11 @@ def index():
 def add_manufacturer():
     form = ManufacturerForm()
     if form.validate_on_submit():
-        manufacturer = Manufacturer(name=form.name.data, street=form.street.data, city=form.city.data,
-                                    zip_code=form.zip_code.data, country=form.country.data)
+        manufacturer = Manufacturer(name=form.name.data,
+                                    street=form.street.data,
+                                    city=form.city.data,
+                                    zip_code=form.zip_code.data,
+                                    country=form.country.data)
         db.session.add(manufacturer)
         db.session.commit()
         return redirect(url_for('index'))
@@ -143,60 +188,44 @@ def add_manufacturer():
 @app.route('/add_model', methods=['GET', 'POST'])
 def add_model():
     form = CarModelForm()
+
+    manufacturers = Manufacturer.query.all()  # Získání seznamu výrobců pro formulář
+    # Nastavení výběru výrobce:
+    form.manufacturer.choices = [(manufacturer.id, manufacturer.name) for manufacturer in manufacturers]
+
     if form.validate_on_submit():
-        car_model = CarModel(name=form.name.data, category=form.category.data, price_range=form.price_range.data,
-                             release_year=form.release_year.data, is_active=form.is_active.data)
+        car_model = CarModel(manufacturer_id=form.manufacturer.data,  # Přiřazení id výrobce
+                             name=form.name.data,
+                             category=form.category.data,
+                             price_range=form.price_range.data,
+                             release_year=form.release_year.data,
+                             is_active=form.is_active.data)
         db.session.add(car_model)
         db.session.commit()
         return redirect(url_for('index'))
-    return render_template('add_model.html', form=form)
+    # Předání seznamu výrobců do šablony
+    return render_template('add_model.html', form=form, manufacturers=manufacturers)
 
 
 # Přidání/úprava automobilu
 @app.route('/add_car', methods=['GET', 'POST'])
 def add_car():
     form = CarForm()
+
+    car_models = CarModel.query.all()
+    form.car_model.choices = [(car_model.id, car_model.name) for car_model in car_models]
+
     if form.validate_on_submit():
-        car = Car(color=form.color.data, horsepower=form.horsepower.data, consumption=form.consumption.data,
-                  manufacture_date=form.manufacture_date.data, is_drivable=form.is_drivable.data)
+        car = Car(model_id=form.car_model.data,
+                  color=form.color.data,
+                  horsepower=form.horsepower.data,
+                  consumption=form.consumption.data,
+                  manufacture_date=form.manufacture_date.data,
+                  is_drivable=form.is_drivable.data)
         db.session.add(car)
         db.session.commit()
         return redirect(url_for('index'))
-    return render_template('add_car.html', form=form)
-
-
-#    def add_cars():
-#        manufacturers = Manufacturer.query.all()
-#        car_models = CarModel.query.all()
-#
-#        if request.method == 'POST':
-#            manufacturer_id = int(request.form.get('manufacturer'))
-#            model_id = int(request.form.get('model'))
-#
-#            manufacturer = Manufacturer.query.get(manufacturer_id)
-#            car_model = CarModel.query.get(model_id)
-#
-#            if not manufacturer or not car_model:
-#                flash('Invalid manufacturer or car model selected.', 'error')
-#            else:
-#                file = request.files['car_data']
-#                if file and file.filename.endswith('.json'):
-#                    try:
-#                        cars_data = json.load(file)
-#                        for car_data in cars_data:
-#                            car = Car(model_id=model_id, color=car_data['color'], horsepower=car_data['horsepower'],
-#                                      consumption=car_data['consumption'],
-#                                      manufacture_date=car_data['manufacture_date'],
-#                                      is_drivable=car_data['is_drivable'])
-#                            db.session.add(car)
-#                        db.session.commit()
-#                        return redirect(url_for('index'))
-#                    except json.JSONDecodeError:
-#                        flash('Invalid JSON file format.', 'error')
-#                else:
-#                    flash('Please upload a valid JSON file.', 'error')
-#
-#        return render_template('add_cars.html', manufacturers=manufacturers, car_models=car_models)
+    return render_template('add_car.html', form=form, car_models=car_models)
 
 
 if __name__ == '__main__':
